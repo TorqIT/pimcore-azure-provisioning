73e4099abb5512b1761f345f6063ace053cc7d28
Evan Jackson <evan@jacksonnet.ca>
2023-04-11 16:11:41 -0300

    Fixing refs to resourceGroupName

--- create-key-vault.sh
+++ create-key-vault.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-RESOURCE_GROUP=$(jq -r '.parameters.resourceGroup.value' $1)
+RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' $1)
 LOCATION=$(jq -r '.parameters.location.value' $1)
 KEY_VAULT_NAME=$(jq -r '.parameters.keyVaultName.value' $1)
 
