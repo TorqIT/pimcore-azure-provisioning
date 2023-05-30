5d30b39d4d6f731df7ff8a6224313ad488a965e1
Evan Jackson <evan@torqit.ca>
2023-05-30 16:36:54 -0300

    Adjusting usage

--- scripts/download-assets.sh
+++ scripts/download-assets.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Usage: ./download-assets.sh <path to parameters.json file> <destination path>
+# If you run this from within a container, you can copy the results to your host OS by exiting the container and running `docker cp -r <destination path on host OS> <container name>:<path inside container>`
+
 STORAGE_ACCOUNT=$(jq -r '.parameters.storageAccountName' $1)
 CONTAINER=$(jq -r '.parameters.storageAccountAssetsContainerName' $1)
 KEY=$(az storage account keys list --acount-name $STORAGE_ACCOUNT | jq -r '.[0].value')
@@ -9,5 +12,5 @@ az storage blob directory download \
     --acount-key $KEY \
     --container $CONTAINER \
     --source-path assets \
-    --destination-path $3 \
+    --destination-path $2 \
     --recursive
