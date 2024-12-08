c753d7ffa1431371149a2e43bace260bc100ef26
Evan Jackson <evan.jackson@torqit.ca>
2024-12-08 16:03:27 -0500

    Service principal now gets Contributor role on RG

--- provision.sh
+++ provision.sh
@@ -73,21 +73,12 @@ then
     SERVICE_PRINCIPAL_ID=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].{spID:id}" --output tsv)
   fi
 
-  PROVISION_INIT=$(jq -r '.parameters.provisionInit.value // empty' $1)
-  INIT_CONTAINER_APP_JOB_NAME=$(jq -r '.parameters.initContainerAppJobName.value // empty' $1)
-  PHP_CONTAINER_APP_NAME=$(jq -r '.parameters.phpContainerAppName.value' $1)
-  SUPERVISORD_CONTAINER_APP_NAME=$(jq -r '.parameters.supervisordContainerAppName.value' $1)
   echo "Assigning roles for service principal..."
   az deployment group create \
     --resource-group $RESOURCE_GROUP \
     --template-file ./bicep/service-principal/service-principal-roles.bicep \
     --parameters \
       servicePrincipalId=$SERVICE_PRINCIPAL_ID \
-      containerRegistryName=$CONTAINER_REGISTRY_NAME \
-      provisionInit=${PROVISION_INIT:-false} \
-      initContainerAppJobName=$INIT_CONTAINER_APP_JOB_NAME \
-      phpContainerAppName=$PHP_CONTAINER_APP_NAME \
-      supervisordContainerAppName=$SUPERVISORD_CONTAINER_APP_NAME
 fi
 
 echo "Done!"
