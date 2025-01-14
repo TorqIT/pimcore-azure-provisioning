278731d740bcf9aa8ab20e3a44d2009a9b9d033d
Evan Jackson <evan.jackson@torqit.ca>
2025-01-13 23:39:04 -0500

    Separate CR provisioning to script and only run if CR does not yet exist

--- provision.sh
+++ provision.sh
@@ -21,17 +21,7 @@ fi
 # Because we need to run some non-Bicep scripts after deploying the Container Registry (but before
 # deploying the other resources), we create the Container Registry separately here before running the
 # main Bicep file.
-echo "Deploying Container Registry..."
-CONTAINER_REGISTRY_NAME=$(jq -r '.parameters.containerRegistryName.value' $1)
-CONTAINER_REGISTRY_SKU=$(jq -r '.parameters.containerRegistrySku.value // empty' $1)
-az deployment group create \
-  --resource-group $RESOURCE_GROUP \
-  --template-file ./bicep/container-registry/container-registry.bicep \
-  --parameters \
-    containerRegistryName=$CONTAINER_REGISTRY_NAME \
-    sku="${CONTAINER_REGISTRY_SKU:-Basic}"
-./bicep/container-registry/push-images.sh $1
-./bicep/container-registry/purge-container-registry-task.sh $1
+. ./provisioning-scripts/provision-container-registry.sh $1
 
 echo "Provisioning the rest of the Azure environment..."
 az deployment group create \
