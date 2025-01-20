00a4a0c7bbed0180551a7c94c62db0bc65fb4bbe
Evan Jackson <evan.jackson@torqit.ca>
2025-01-20 17:05:21 -0500

    Add error handling to provision-container-registry.sh

--- provisioning-scripts/provision-container-registry.sh
+++ provisioning-scripts/provision-container-registry.sh
@@ -1,9 +1,11 @@
 RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' $1)
 CONTAINER_REGISTRY_NAME=$(jq -r '.parameters.containerRegistryName.value' $1)
 
+set +e
 az acr show \
   --resource-group $RESOURCE_GROUP \
   --name $CONTAINER_REGISTRY_NAME > /dev/null 2>&1
+set -e
 
 if [ $? -ne 0 ]; then
   echo "Deploying Container Registry..."
