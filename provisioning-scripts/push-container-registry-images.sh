0b3f5d32717a98711540c135c4fe1f2cd5a45d6e
Evan Jackson <evan.jackson@torqit.ca>
2025-04-24 13:03:01 -0400

    Move network steps otuside

--- provisioning-scripts/push-container-registry-images.sh
+++ provisioning-scripts/push-container-registry-images.sh
@@ -10,13 +10,16 @@ SUPERVISORD_IMAGE_NAME=$(jq -r '.parameters.supervisordContainerAppImageName.val
 
 IMAGES=($PHP_IMAGE_NAME $SUPERVISORD_IMAGE_NAME $INIT_IMAGE_NAME)
 
+if [ "$CONTAINER_REGISTRY_SKU" == "Premium" ]; then
+  echo Adding temporary network rule to the Container Registry firewall...
+  az acr network-rule add -n $CONTAINER_REGISTRY_NAME --ip-address $(curl ipinfo.io/ip)
+fi
+
+echo "Checking if Container registry has necessary repositories..."
 EXISTING_REPOSITORIES=$(az acr repository list --name $CONTAINER_REGISTRY_NAME --output tsv)
+
 if [ -z "$EXISTING_REPOSITORIES" ];
 then
-  if [ "$CONTAINER_REGISTRY_SKU" == "Premium" ]; then
-    echo Adding temporary network rule to the Container Registry firewall...
-    az acr network-rule add -n $CONTAINER_REGISTRY_NAME --ip-address $(curl ipinfo.io/ip)
-  fi
 
   # Container Apps require images to actually be present in the Container Registry,
   # therefore we tag and push some dummy Hello World ones here.
@@ -27,12 +30,11 @@ then
     docker tag hello-world:latest $CONTAINER_REGISTRY_NAME.azurecr.io/$image:latest
     docker push $CONTAINER_REGISTRY_NAME.azurecr.io/$image:latest
   done
-  docker logout
-
-  if [ "$CONTAINER_REGISTRY_SKU" == "Premium" ]; then
-    echo Removing temporary network rule from the Container Registry firewall...
-    az acr network-rule remove -n $CONTAINER_REGISTRY_NAME --ip-address $(curl ipinfo.io/ip)
-  fi
 else
-  echo "Container Registry repositories already exist ($EXISTING_REPOSITORIES), so no need to push anything"
+  echo "Container Registry repositories already exist ($EXISTING_REPOSITORIES), so no need to push anything!"
+fi
+
+if [ "$CONTAINER_REGISTRY_SKU" == "Premium" ]; then
+  echo Adding temporary network rule to the Container Registry firewall...
+  az acr network-rule add -n $CONTAINER_REGISTRY_NAME --ip-address $(curl ipinfo.io/ip)
 fi
